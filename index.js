c328e960e9b4148250b02cb5476cae18999b0f0d
Zongmin Lei <leizongmin@gmail.com>
2015-10-07 10:20:49 +0800

    simpleRequest()

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * lei-crawler
+ *
+ * @author Zongmin Lei <leizongmin@gmail.com>
+ */
+
+/*
+var request = simpleRequest();
+function callback (err, body) {
+  console.log(err, body && body.length);
+}
+request({url: 'http://baidu.com'}, callback);
+request({url: 'http://baidu.com'}, callback);
+request({url: 'http://baidu.com'}, callback);
+*/
+exports.simpleRequest = require('./lib/request');
+
+
