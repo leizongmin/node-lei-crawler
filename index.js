e400513276859b478bda38922e85744760bdf86b
Zongmin Lei <leizongmin@gmail.com>
2015-10-09 11:43:32 +0800

    forEachURL()

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ var utils = require('./lib/utils');
 var simpleRequest = require('./lib/request');
 var simpleStore = require('./lib/store');
 var collectURLs = require('./lib/collect_urls');
+var forEachURL = require('./lib/foreach_url');
 
 exports.utils = utils;
 exports.simpleRequest = simpleRequest;
 exports.simpleStore = simpleStore;
 exports.collectURLs = collectURLs;
+exports.forEachURL = forEachURL;
 
 
 /*
@@ -40,5 +42,18 @@ var store = simpleStore({db: 8});
 store.sAdd('a', [1, 2, 3, 4], function (err) {
   if (err) throw err;
   store.sInter('a', [1, 3, 5, 7], console.log);
+});
+*/
+
+/*
+forEachURL({
+  name: 't20151007',
+  store: exports.simpleStore({db: 8})
+}).start(function (url, next) {
+  console.log('URL: %s', url);
+  next();
+}, function (err) {
+  console.log('done [err=%s]', err);
+  process.exit();
 });
 */
